6419590148cf2eff97f373aacb451957d804ac8a
lowbattery1800 <lowbattery1800@gmail.com>
2024-09-28 10:50:23 -0300

    some fork test 2

--- python-api/app/main.py
+++ python-api/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 @app.get("/")
 async def read_main():
     """This is the main message"""
-    return {"msg": "Hello World 5"}
+    return {"msg": "Hello World 6"}
 
 
 @app.get("/another")
