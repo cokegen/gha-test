e25dbd1d16d6f355708aa021588b955669b7a88d
Carlos Castillo <cokegen@gmail.com>
2024-09-28 09:47:22 -0300

    Initial commit

---
+++ python-api/app/main.py
@@ -0,0 +1,17 @@
+"""This is the app message"""
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_main():
+    """This is the main message"""
+    return {"msg": "Hello World 3"}
+
+
+@app.get("/another")
+async def read_another():
+    """This is the alternate message"""
+    return {"msg": "ANOTHER World"}
